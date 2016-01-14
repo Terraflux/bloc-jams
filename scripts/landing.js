b546859acb85968dd85fbe8219060a429641de07
Pedro Urbina <pedro.urbina@gmail.com>
2016-01-14 18:32:05 -0500

    checkpoint 23 work

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,18 +1,29 @@
-var animatePoints = function(){
+var points = document.getElementsByClassName('point');
 
+var animatePoints = function(points){
 	var revealPoint = function(i){
 			points[i].style.opacity = 1;
 			points[i].style.transform = "scaleX(1) translateY(0)";
 			points[i].style.msTransform = "scaleX(1) translateY(0)";
 			points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
 		}
-
-
-	var points = document.getElementsByClassName('point');
-	
 	for (var i in points){
-		revealPoint(i)
+		revealPoint(i);
 	}
 };
 
-animatePoints();
+window.onload = function(){
+
+	if (window.innerHeight > 950) {
+		animatePoints(points);
+	}
+
+	var sellingPoints = document.getElementsByClassName('selling-points')[0];
+	var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+	window.addEventListener('scroll', function(event){
+		if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+			animatePoints(points);
+		}
+	});
+}
