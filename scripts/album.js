56f467c5497b9d68c7c34a4afdd5639cf8fa1256
Pedro Urbina <pedro.urbina@gmail.com>
2016-01-22 14:25:45 -0500

    checkpoint 27 assignment

--- scripts/album.js
+++ scripts/album.js
@@ -86,13 +86,29 @@ var setCurrentAlbum = function(album){
     currentAlbum = album;
 };
 
+
 var findParentByClassName = function(element, targetClass){
     var currentParent = element.parentElement;
+
+    if (currentParent){
+        while (currentParent.className && currentParent.className != targetClass){
+            currentParent = currentParent.parentElement;
+        }
+        if (currentParent.className == targetClass){
+            return currentParent;
+        } else {
+            alert("No parent with that class name found.");
+        }
+    } else {
+        alert("No parent found.")
+    }
+
     while (currentParent.className != targetClass){
         currentParent = currentParent.parentElement;
     }
     return currentParent;
 };
+
 
 var getSongItem = function(element){
     switch (element.className){
